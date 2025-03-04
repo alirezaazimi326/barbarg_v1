129e16d72038969eee063ab433ff614c47da9d76
alireza linux <alirezaazimi326@gmail.com>
2025-03-04 12:47:51 +0330

    feat: Add drivers doctype

---
+++ barbarg_v1/barbarg/doctype/drivers/drivers.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2024, Barbarg and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+class Drivers(Document):
+    pass 
